e36bf27e11a050df7eb18754a42fe6eb58bcf29f
Jeremías <jereecalvet@gmail.com>
2023-01-10 16:31:02 -0300

    new host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-argentina-programa7.herokuapp.com/api/v1/',
+  apiUrl: 'https://backendproyectoargprog.onrender.com/api/v1/',
 };
